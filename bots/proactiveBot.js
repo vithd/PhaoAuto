f4cda2ece4c2bdb84b903ca0f1ad3338f6965775
VietHD <viethd@gearinc.com>
2019-09-13 14:09:56 +0700

    Add welcome& help messages

--- bots/proactiveBot.js
+++ bots/proactiveBot.js
@@ -2,6 +2,15 @@
 // Licensed under the MIT License.
 
 const { ActivityHandler, TurnContext } = require('botbuilder');
+const sprintf = require('sprintf-js').sprintf;
+const CronJob = require('cron').CronJob;
+
+/*
+const order = new CronJob('* 30 10 * * *', function() {
+  console.log('You will see this message every second');
+  order.stop();
+}, null, true, 'America/Los_Angeles');
+*/
 
 class ProactiveBot extends ActivityHandler {
     constructor(conversationReferences) {
@@ -20,7 +29,7 @@ class ProactiveBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; cnt++) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    const welcomeMessage = 'Welcome to the Proactive Bot sample.  Navigate to http://localhost:3978/api/notify to proactively message everyone who has previously messaged this bot.';
+                    const welcomeMessage = this.getHelpMessage(context.activity.from.name);
                     await context.sendActivity(welcomeMessage);
                 }
             }
@@ -31,9 +40,17 @@ class ProactiveBot extends ActivityHandler {
 
         this.onMessage(async (context, next) => {
             this.addConversationReference(context.activity);
+            const text = context.activity.text.toLowerCase().trim();
 
-            // Echo back what the user said
-            await context.sendActivity(`You sent '${ context.activity.text }'`);
+            // Help
+            if (text.indexOf('help') 
+                || text.indexOf('how')
+                || text.indexOf('?') 
+                || text === 'pháo tự động'
+            ) {
+                await context.sendActivity(this.getHelpMessage(context.activity.from.name));
+            }
+
             await next();
         });
     }
@@ -41,6 +58,25 @@ class ProactiveBot extends ActivityHandler {
     addConversationReference(activity) {
         const conversationReference = TurnContext.getConversationReference(activity);
         this.conversationReferences[conversationReference.conversation.id] = conversationReference;
+
+        console.log(conversationReference);
+    }
+
+    getHelpMessage(name) {
+        return `Xin chào ${name}! Đây là group Cơm Pháo~ Hàng ngày vào buổi sáng chị chủ Pháo sẽ gửi cơm và hẹn giờ chốt cơm. Em sẽ mở đăng ký đến giờ chốt cơm, nhận tiền và báo nợ thay mặt chị chủ.
+
+Cách đặt cơm: @phaotudong [so luong] [ghi chu]
+Ví dụ: @Pháo Tự Động 2 nhiều thịt ít rau
+Cách trả tiền: Bỏ tiền vào hộp tiền Cơm Nhà Pháo,tag Pháo Tự Động, thêm chữ x, ghi momo nếu dùng Momo
+Ví dụ "@Pháo Tự Động x" hoặc "@Pháo Tự Động x momo"
+
+Hallo ${name}! This is Cơm Pháo lunch group~ Everyday in morning my master will send lunch menu. I'll open registration, take payment and remind paying at the end of the day.
+
+How to order: @Pháo Tự Động [quatity] [note]
+Ex: @Pháo Tự Động 2 nhiều thịt ít rau (It means "more meat less veget")
+
+How to pay: Put money in Cơm Nhà Pháo’s money box, tag Pháo Tự Động with an x
+Ex: @Pháo Tự Động x`;
     }
 }
 
